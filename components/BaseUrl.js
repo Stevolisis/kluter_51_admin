fb56e8929896948b99580bbdc84b770da86a9738
Steven Joseph <stevolisisjoseph@gmail.com>
2023-02-09 14:38:29 +0100

    .env variables

--- components/BaseUrl.js
+++ components/BaseUrl.js
@@ -1,8 +1,8 @@
 
 const phpUrl='http://localhost';
 
-const baseUrl='http://localhost:3000';
+const baseUrl2='http://localhost:3000';
 const baseUrl3='https://main--reliable-tapioca-719734.netlify.app';
-const baseUrl2='https://ototechblog.vercel.app';
+const baseUrl='https://techreveal.vercel.app';
 
 module.exports={baseUrl,phpUrl}

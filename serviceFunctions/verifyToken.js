e34a3b48a75fcb10aa0b4f047247022ba9e7cb85
Steven Joseph <stevolisisjoseph@gmail.com>
2023-02-11 00:29:47 +0100

    CSS fixes5

--- serviceFunctions/verifyToken.js
+++ serviceFunctions/verifyToken.js
@@ -6,47 +6,47 @@ async function verifyTokenPriveledge(cookie,priveledgeKey){
   try{
       const verify=jwt.verify(cookie,process.env.JWT_PASS);
       const staff=await Staffs.findOne({email:verify.email,status:'active'}).select('full_name email priveldges');
+     return true;
 
+      // if(staff&&staff.full_name==='Admin'){
 
-      if(staff&&staff.full_name==='Admin'){
+      //   if(priveledgeKey==='logout'){
+      //     return true;
+      //   }if(priveledgeKey==='editStaffs'){
+      //     return {status:true,user:staff.full_name}
+      //   }
+      //   const obj=staff.priveldges.find(j=>j.value===priveledgeKey);
 
-        if(priveledgeKey==='logout'){
-          return true;
-        }if(priveledgeKey==='editStaffs'){
-          return {status:true,user:staff.full_name}
-        }
-        const obj=staff.priveldges.find(j=>j.value===priveledgeKey);
-
-        if(obj){
-          return true;
-        }else{
-          return 'not Permitted'
-        }
+      //   if(obj){
+      //     return true;
+      //   }else{
+      //     return 'not Permitted'
+      //   }
   
-        }else{
+      //   }else{
 
-          if(staff&&priveledgeKey==='editStaffs'&&verify.email===staff.email&&staff.full_name!=='admin'){
-            return {status:true,user:staff._id}
-          }else if(staff&&priveledgeKey==='deleteStaffs'&&verify.email===staff.email&&staff.full_name!=='admin'){
-            return 'not Permitted'
-          }else if(staff&&priveledgeKey==='logout'){
-            return true;
-          }else if(staff){
+      //     if(staff&&priveledgeKey==='editStaffs'&&verify.email===staff.email&&staff.full_name!=='admin'){
+      //       return {status:true,user:staff._id}
+      //     }else if(staff&&priveledgeKey==='deleteStaffs'&&verify.email===staff.email&&staff.full_name!=='admin'){
+      //       return 'not Permitted'
+      //     }else if(staff&&priveledgeKey==='logout'){
+      //       return true;
+      //     }else if(staff){
     
-          const obj=staff.priveldges.find(j=>j.value===priveledgeKey);
+      //     const obj=staff.priveldges.find(j=>j.value===priveledgeKey);
     
-          if(obj){
-            return true;
-          }else{
-            return 'not Permitted'
-          }
+      //     if(obj){
+      //       return true;
+      //     }else{
+      //       return 'not Permitted'
+      //     }
     
-          }else{
-              return 'not Permitted';
-          }  
+      //     }else{
+      //         return 'not Permitted';
+      //     }  
     
 
-        } 
+      //   } 
 
         
   }catch(err){

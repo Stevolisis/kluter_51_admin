449d098f9cd3b841c4a1068ca2b915cd9612c667
Steven Joseph <stevolisisjoseph@gmail.com>
2023-06-09 15:46:29 +0100

    404 html removed

--- pages/[blogCategory]/index.js
+++ pages/[blogCategory]/index.js
@@ -29,7 +29,7 @@ export const getStaticPaths=async()=>{
               console.log('categoryyyyyyyyyyy',category)
               return {
                   params:{
-                      blogCategory:category.slug.split('/')[0]||"404"
+                      blogCategory:category.slug.split('/')[0]
                   }
               }
           }),

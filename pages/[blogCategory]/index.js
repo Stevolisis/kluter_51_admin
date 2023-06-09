71269061885325c781d36dbdfa06afc5bbe0ae32
Steven Joseph <stevolisisjoseph@gmail.com>
2023-06-09 15:59:02 +0100

    category name added as fallback

--- pages/[blogCategory]/index.js
+++ pages/[blogCategory]/index.js
@@ -29,7 +29,7 @@ export const getStaticPaths=async()=>{
               console.log('categoryyyyyyyyyyy',category)
               return {
                   params:{
-                      blogCategory:category.slug.split('/')[0]
+                      blogCategory:category.slug.split('/')[0]||"Startups"
                   }
               }
           }),

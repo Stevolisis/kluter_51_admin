5871d23fb845665f2ebfae38b26321c3ec539a75
Steven Joseph <stevolisisjoseph@gmail.com>
2023-08-03 00:42:34 +0100

    created getSubscribersStat

--- components/BaseUrl.js
+++ components/BaseUrl.js
@@ -1,8 +1,8 @@
 
 const phpUrl='http://localhost';
 
-const baseUrl2='http://localhost:3000';
-const baseUrl='https://techreveal.vercel.app';
+const baseUrl='http://localhost:3000';
+const baseUrl2='https://techreveal.vercel.app';
 const baseUrl3='https://main--reliable-tapioca-719734.netlify.app';
 
 module.exports={baseUrl,phpUrl}

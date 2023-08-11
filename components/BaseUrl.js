eaf3d1dafc919537246e6c2377749ae981e22635
Steven Joseph <stevolisisjoseph@gmail.com>
2023-08-11 22:56:29 +0100

    baseURL changed for render host

--- components/BaseUrl.js
+++ components/BaseUrl.js
@@ -2,7 +2,8 @@
 const phpUrl='http://localhost';
 
 const baseUrl2='http://localhost:3000';
-const baseUrl='https://techreveal.vercel.app';
+const baseUrl='https://techreveal.onrender.com';
+const baseUrl4='https://techreveal.vercel.app';
 const baseUrl3='https://main--reliable-tapioca-719734.netlify.app';
 
 module.exports={baseUrl,phpUrl}
